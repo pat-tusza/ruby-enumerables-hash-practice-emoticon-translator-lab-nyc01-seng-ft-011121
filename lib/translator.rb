8cb5569b2446cd5e7e6d4ba57c856b21f93377f3
Patrick Tuszakowski <mrtall500@gmail.com>
2020-12-30 21:01:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,9 @@ def load_library(file_path)
   lib_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
+  library = load_library(file_path)
+  
   # code goes here
 end
 
